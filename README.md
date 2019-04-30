7eae8e0ea7cf9c1815dc85b65fe5bce24280a023
John Rees <john@bitsushi.com>
2019-04-30 18:54:33 +0100

    form works

---
+++ README.md
@@ -0,0 +1,9 @@
+Deployed with Netlify to https://planx-beta-signup.netlify.com
+
+Hosted with CloudFlare at https://signup.planx.uk
+
+Form gets posted to https://docs.google.com/spreadsheets/d/1HaY0nnRQVRFsLc_CU3baY43DfExAGoIG1ithkqYZ6kg/edit#gid=0
+
+Function to insert form data into spreadsheet https://script.google.com/d/10fW_x1poQsrnemcZmkxUxV_VL_vALb079VoLlJFW0oI7q-yv_X0HXupw/edit?uiv=2&mid=ACjPJvEPmKam1DS113o3_FEgYm2Rd_QFWrAQivnajovf_rcs5QqCVeKF3pOiYaQSdoFP09G37k0ViUmYyzggw_cufZgZzmaLpqykYKrvpDUj7vUK6vnxyAugng2WXSor5FrDUUtsj5jt62HiEKyM3uekMcK-XDo53mf94oX72Kvu7vUIR9qUIZ6pLM0OmQ&splash=yes
+
+Inspired by https://medium.com/@dmccoy/how-to-submit-an-html-form-to-google-sheets-without-google-forms-b833952cc175
--- src/index.js
+++ src/index.js
@@ -1,42 +1,67 @@
+$.fn.serializeObject = function() {
+  "use strict";
+  var a = {},
+    b = function(b, c) {
+      var d = a[c.name];
+      "undefined" != typeof d && d !== null
+        ? $.isArray(d)
+          ? d.push(c.value)
+          : (a[c.name] = [d, c.value])
+        : (a[c.name] = c.value);
+    };
+  return $.each(this.serializeArray(), b), a;
+};
+
 function submitted() {
-   $("#content").fadeOut(() => {
-      $("#thank-you").fadeIn();
-   });
+  $("#content").fadeOut(() => {
+    $("#thank-you").fadeIn();
+  });
 }
 
 $.when($.ready).then(function() {
-   var $body = $("body"),
-      $select = $("#select_label");
+  var $body = $("body"),
+    $select = $("#select_label");
 
-   $("#select_label").on("click", function(e) {
-      e.stopPropagation();
-      if ($select.is(":checked")) {
-         $body.on("click", function(e) {
-            if (!$(e.target).is("#select_label + label")) {
-               $select.prop("checked", false);
-               $body.off("click");
-            }
-         });
-      }
-   });
+  $("#select_label").on("click", function(e) {
+    e.stopPropagation();
+    if ($select.is(":checked")) {
+      $body.on("click", function(e) {
+        if (!$(e.target).is("#select_label + label")) {
+          $select.prop("checked", false);
+          $body.off("click");
+        }
+      });
+    }
+  });
 
-   $("#options label").click(function() {
-      $select.prop("checked", false);
-   });
+  $("#options label").click(function() {
+    $select.prop("checked", false);
+  });
 
-   $("#form").on("submit", e => {
-      e.preventDefault();
-      $.post(
-         "https://webhook.site/481cb5bf-f10a-4355-850c-431ff26d147f",
-         $("#form").serialize()
-      )
-         .done(submitted)
-         .fail(function(jqXHR, textStatus, errorThrown) {
-            console.error("Probably a CORS error...");
-            submitted();
-         })
-         .always(function() {
-            console.log("done");
-         });
-   });
+  $("#form").on("submit", e => {
+    e.preventDefault();
+
+    if (!$("input[name='occupation']:checked").val())
+      return alert("please choose an occupation");
+
+    $("#form").prop("disabled", true);
+    $("#submit").prop("disabled", true);
+
+    const url =
+      "https://script.google.com/macros/s/AKfycbwT_FNQfkN9vJfZnIMkN273_ethCsO5TRPwnI-f8CFnACMFdv8/exec";
+    $.ajax({
+      url,
+      method: "GET",
+      dataType: "json",
+      data: $("#form").serializeObject()
+    })
+      .done(submitted)
+      .fail(function(jqXHR, textStatus, errorThrown) {
+        console.error("Probably a CORS error...");
+        submitted();
+      })
+      .always(function() {
+        console.log("done");
+      });
+  });
 });
